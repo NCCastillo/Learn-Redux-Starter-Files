194f90a3c47d46fb2d9a7f21cd80f40d93987884
Nestor Castillo <nes_23@yahoo.com>
2016-09-26 18:34:13 -0600

    Finish video 10

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,10 +1,10 @@
-//STOPPED ON VIDEO 10
+//STOPPED ON VIDEO 
 import React from 'react';
 
 import { render } from 'react-dom';
 
 import css from './styles/style.styl';
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
@@ -15,7 +15,7 @@ import store, { history } from './store';
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
         <Route path="/view/:postId" component={Single}></Route>
       </Route>
